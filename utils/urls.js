f7024d5cc20614ae7b045580c67b9ac0079eb89b
Bhavi Patel <bhavika.is.patel@gmail.com>
2022-10-21 12:16:28 -0400

    Stripe Pk

--- utils/urls.js
+++ utils/urls.js
@@ -1,9 +1,9 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1338"
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"
 
 export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || 'pk_test_42B2064C668798B5'
 
-export const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PK || 'pk_test_42B2064C668798B5'
+export const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PK || 'pk_test_51Li4KkDLVYbccAO3GumsCgBwi8t4vuKBonFWXoZW1kCCdKojD2kwRA07f7agjKeEXAUjnHfhZUI344fhQ3Qd0dcz00GuOp3l7R'
 
 /**
  * Given a image object return the proper path to display it
